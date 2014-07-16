e232e57ba161b48dbe678985364e2238a6a93784
Naoufal Kadhom <naoufalkadhom@gmail.com>
2014-07-16 13:44:47 +0000

    add sidebar tests for update titles

--- test/casperjs/sidebar.js
+++ test/casperjs/sidebar.js
@@ -1,8 +1,3 @@
-var child_process = require('child_process');
-child_process.spawn('node',  ['app']);
-
-
-
 casper.test.begin('Side menu can be opened', function(test) {
   casper.start('http://127.0.0.1:5000/', function() {
     test.assertExists('#header .btn');
@@ -30,6 +25,25 @@ casper.test.begin('Side menu can be dismissed with overlay', function(test) {
       this.click('.overlay');
       test.assertDoesntExist('.sidebar.is-visible');
       test.pass('.sidebar dismissed with overlay');
+    });
+  })
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+
+casper.test.begin('Clicking category updates title', function(test) {
+  casper.start('http://127.0.0.1:5000/', function() {
+    this.click('#header .btn');
+    this.waitUntilVisible('.sidebar.is-visible', function() {
+      console.log(this);
+      test.assertElementCount('.category', 4)
+      test.assertSelectorHasText('.category-list li:nth-of-type(3) a', 'New Releases')
+      this.click('.category-list li:nth-of-type(3) a');
+      test.assertSelectorHasText('title', 'New Releases | Flix')
+      test.assertSelectorHasText('#header .title', 'New Releases')
     });
   })
 
