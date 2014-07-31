2eec3e0844e9599c03193ed71df0b93ac10d3b72
Naoufal Kadhom <naoufalkadhom@gmail.com>
2014-07-30 23:16:07 -0400

    add mongodb url to defaults

--- config/defaults.js
+++ config/defaults.js
@@ -5,6 +5,7 @@ module.exports = {
   PORT: 5000,   // port to run the API server on
 
   REDIS_URL: process.env.REDISCLOUD_URL || 'redis://localhost:6379',
+  MONGO_URL: process.env.MONGOHQ_URL || 'mongodb://localhost/flix',
 
   THEMOVIEDB_KEY: 'OVERWRITE WITH LOCAL FILE',
   ROTTENTOMATOES_KEY: 'OVERWRITE WITH LOCAL FILE'
